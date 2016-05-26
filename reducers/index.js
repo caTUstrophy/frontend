bed344b13a5ff712928e66f30984ef85522d20e8
neopostmodern <clemens@neopostmodern.com>
2016-05-26 02:23:15 +0200

    Delete unnecessary code from example

--- reducers/index.js
+++ reducers/index.js
@@ -29,22 +29,6 @@ function errorMessage(state = null, action) {
 
 // Updates the pagination data for different actions.
 const pagination = combineReducers({
-  // starredByUser: paginate({
-  //   mapActionToKey: action => action.login,
-  //   types: [
-  //     ActionTypes.STARRED_REQUEST,
-  //     ActionTypes.STARRED_SUCCESS,
-  //     ActionTypes.STARRED_FAILURE
-  //   ]
-  // }),
-  // stargazersByRepo: paginate({
-  //   mapActionToKey: action => action.fullName,
-  //   types: [
-  //     ActionTypes.STARGAZERS_REQUEST,
-  //     ActionTypes.STARGAZERS_SUCCESS,
-  //     ActionTypes.STARGAZERS_FAILURE
-  //   ]
-  // })
 })
 
 const rootReducer = combineReducers({
