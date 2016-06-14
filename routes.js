127479c38d30d40a0779ff9712394432a14e24de
Tom Lord <design@tomlord.eu>
2016-06-14 13:09:29 +0200

    Users can view own offers and requests

--- routes.js
+++ routes.js
@@ -20,6 +20,8 @@ import SignupPage from './containers/user/SignupPage'
 // other
 import DefaultPage from './containers/misc/EmptyPage'
 import NotFoundPage from './containers/misc/NotFoundPage'
+import MyOffersPage from "./containers/me/MyOffersPage";
+import MyRequestsPage from "./containers/me/MyRequestsPage";
 
 
 export default [
@@ -43,6 +45,17 @@ export default [
       </Route>
       <Route path="requests">
         <IndexRoute component={RequestsPage} />
+        <Route path=":ID" component={RequestPage} />
+      </Route>
+    </Route>
+    <Route path="me">
+      <IndexRoute component={DefaultPage} />
+      <Route path="offers">
+        <IndexRoute component={MyOffersPage} />
+        <Route path=":ID" component={OfferPage} />
+      </Route>
+      <Route path="requests">
+        <IndexRoute component={MyRequestsPage} />
         <Route path=":ID" component={RequestPage} />
       </Route>
     </Route>
