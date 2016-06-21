a27ce1a0a0745748db9fdc27b87b4f8d041460fc
neopostmodern <clemens@neopostmodern.com>
2016-06-21 13:40:28 +0200

    More attempts to fix build (HMR still?)

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -10,7 +10,6 @@ const environmentPlugin = new webpack.DefinePlugin({
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-hot-middleware/client',
     './index'
   ],
   output: {
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ var webpack = require('webpack');
 
 var config = require("./webpack.base.config.js");
 
+config.entry.unshift('webpack-hot-middleware/client');
+
 //Get a handle the base config's plugins array and add the plugin
 config.plugins = config.plugins || [ ];
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
