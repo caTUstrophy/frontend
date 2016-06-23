d4d7090498b1d0201c6311fd35eae8b309bac3db
neopostmodern <clemens@neopostmodern.com>
2016-06-24 01:37:56 +0200

    Basic user interface for adding and displaying region admins, no functionality yet

--- components/regions/Region.js
+++ components/regions/Region.js
@@ -1,11 +1,16 @@
 import React, { Component, PropTypes } from 'react'
+import { reset } from 'redux-form';
+
+import autobind from 'autobind-decorator'
 
 import { Card, CardHeader, CardText, CardActions } from 'material-ui/Card'
+import { List, ListItem } from 'material-ui/List'
 import FlatButton from 'material-ui/FlatButton'
 
 import FingerprintIcon from 'material-ui/svg-icons/action/fingerprint'
 import CropFreeIcon from 'material-ui/svg-icons/image/crop-free'
 
+import InlineEmailForm, { INLINE_EMAIL_FORM_ID } from '../../forms/InlineEmailForm'
 import SimpleMap from '../maps/SimpleMap'
 import { geodesicArea } from '../../helpers/Location';
 
@@ -17,6 +22,20 @@ export default class Region extends Component {
     onClickRequests: PropTypes.func.isRequired,
     onClickOffers: PropTypes.func.isRequired
   };
+
+  @autobind
+  handleEmailSubmit(email) {
+    alert(`Add "${email.Mail}" as admin to region ${this.props.region.ID}`);
+    // todo: dispatch(reset(INLINE_EMAIL_FORM_ID)) - but component shouldn't handle redux ops
+  }
+
+  renderAdminList() {
+    return <List>
+      <ListItem key="empty"
+                primaryText="No administrators for this region"
+                disabled={true}/>
+    </List>;
+  }
 
   render() {
     const region = this.props.region;
@@ -31,6 +50,10 @@ export default class Region extends Component {
         </CardText>
         <SimpleMap area={region.Boundaries.Points} style={{'height': '400px'}} />
         <CardText>
+          <h4>Administration</h4>
+          {this.renderAdminList()}
+          <InlineEmailForm onSubmit={this.handleEmailSubmit} submitButtonText="Add user to admins" ref="add-admin-email-form" />
+          <h4 style={{marginTop: '2rem'}}>Details</h4>
           <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem'}}>
             <CropFreeIcon style={{marginRight: '0.5rem'}} /> {(geodesicArea(region.Boundaries.Points) / 1e6).toFixed(2)}km²
           </div>
