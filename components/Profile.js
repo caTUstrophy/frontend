ac7f43bd32e9aecbad81e773f7b98c1c50a4ef27
neopostmodern <clemens@neopostmodern.com>
2016-06-25 15:17:32 +0200

    Display permissions in profile

--- components/Profile.js
+++ components/Profile.js
@@ -5,6 +5,10 @@ import PhoneIcon from 'material-ui/svg-icons/communication/phone'
 import VerifiedIcon from 'material-ui/svg-icons/action/verified-user'
 import { Card, CardHeader, CardText } from 'material-ui/Card'
 
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export default class Profile extends Component {
   renderPhoneNumbers(numbers) {
     if (numbers.length === 0) {
@@ -14,10 +18,32 @@ export default class Profile extends Component {
       </div>;
     }
 
-    return numbers.map(number => <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem'}}>
-      <PhoneIcon style={{marginRight: '0.5rem'}} />
-      {number}
-    </div>)
+    return numbers.map((number, index) =>
+      <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem'}}
+           key={index}>
+        <PhoneIcon style={{marginRight: '0.5rem'}} />
+        {number}
+      </div>
+    )
+  }
+
+  renderPermissions(groups) {
+    return <div>
+      <h3 style={{marginTop: '2rem'}}>Permissions</h3>
+      {groups.map((group, index) => {
+        let region;
+        if (group.Region.Name) {
+          region = `in region "${group.Region.Name}"`; // todo: link to region?
+        } else {
+          region = <i>globally</i>;
+        }
+
+        return <div key={index}>
+          <b>{capitalizeFirstLetter(group.Permissions.map(permission => permission.AccessRight).join(', '))}</b> &nbsp;
+          {region}
+        </div>
+      })}
+    </div>
   }
 
   render() {
@@ -25,8 +51,6 @@ export default class Profile extends Component {
 
     return (
       <Card>
-        <CardHeader style={{backgroundColor: 'lightgray'}}
-                    title={`${profile.Name}'s profile`} />
         <CardText>
           <h2>{profile.Name} {profile.PreferredName && profile.PreferredName.length ? `(${profile.PreferredName})` : ''}</h2>
           <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem'}}>
@@ -35,8 +59,8 @@ export default class Profile extends Component {
             <span style={{color: 'gray'}}>{profile.MailVerified ? <VerifiedIcon /> : <i>not verified</i>}</span>
           </div>
           {this.renderPhoneNumbers(profile.PhoneNumbers)}
-          <h3 style={{marginTop: '2rem'}}>Permissions</h3>
-          <pre>{JSON.stringify(profile.Groups, null, 2)}</pre>
+
+          {this.renderPermissions(profile.Groups)}
         </CardText>
       </Card>
     )
