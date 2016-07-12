0f283f6016e9306d827c70a01713ce2a1faed78f
neopostmodern <clemens@neopostmodern.com>
2016-07-12 12:02:54 +0200

    Added FAB to my offers and my requests page for quick adding of new post

--- containers/me/MyOffersPage.js
+++ containers/me/MyOffersPage.js
@@ -4,20 +4,27 @@ import { browserHistory } from 'react-router'
 
 import autobind from 'autobind-decorator'
 
+import FloatingActionButton from 'material-ui/FloatingActionButton'
+import ContentAdd from 'material-ui/svg-icons/content/add'
+
 import { loadUserOffers } from '../../actions/offers'
 import OfferList from '../../components/OfferList'
+import {OfferPropType} from "../../schemas/OfferSchema";
 
-function loadData(props) {
-  props.loadUserOffers();
-}
+import Center from '../layout/Center'
 
 class MyOffersPage extends Component {
+  static propTypes = {
+    offers: PropTypes.arrayOf(OfferPropType).isRequired,
+    loadOffers: PropTypes.func.isRequired
+  };
+  
   constructor(props) {
     super(props);
   }
 
   componentWillMount() {
-    loadData(this.props)
+    this.props.loadUserOffers();
   }
   
   @autobind
@@ -32,21 +39,19 @@ class MyOffersPage extends Component {
     }
 
     return (
-      <div>
+      <Center>
         <h1>Your Offers</h1>
         <OfferList offers={offers} onTouchTapItem={this.handleTouchTapItem} />
-      </div>
+  
+        <FloatingActionButton style={{position: 'fixed', bottom: '2rem', right: '2rem'}}
+                              secondary={true}
+                              onTouchTap={() => browserHistory.push('/offers/create')}>
+          <ContentAdd />
+        </FloatingActionButton>
+      </Center>
     )
   }
 }
-
-MyOffersPage.propTypes = {
-  offers: PropTypes.arrayOf(PropTypes.shape({
-    ID: PropTypes.string.isRequired,
-    Name: PropTypes.string.isRequired
-  })).isRequired,
-  loadUserOffers: PropTypes.func.isRequired
-};
 
 function mapStateToProps(state, ownProps) {
   const { entities: { offers } } = state;
