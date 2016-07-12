db1c1a13162a9c16db9b15a06e5ce35232096cab
neopostmodern <clemens@neopostmodern.com>
2016-07-12 14:11:05 +0200

    Display tags in single post view

--- components/Offer.js
+++ components/Offer.js
@@ -4,6 +4,7 @@ import AccountIcon from 'material-ui/svg-icons/action/account-circle'
 import LocationIcon from 'material-ui/svg-icons/communication/location-on'
 import TimerIcon from 'material-ui/svg-icons/image/timer'
 import LabelIcon from 'material-ui/svg-icons/action/label'
+import Chip from 'material-ui/Chip';
 
 import { OfferPropType } from '../schemas/OfferSchema'
 import { toString } from "../helpers/Location"
@@ -26,7 +27,11 @@ export default class Offer extends Component {
           <LocationIcon style={{marginRight: '0.5rem'}} /> {toString(offer.Location)}
         </div>
         <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem'}}>
-          <LabelIcon style={{marginRight: '0.5rem'}} /> <i>Untagged</i>
+          <LabelIcon style={{marginRight: '0.5rem'}} />
+          {offer.Tags && offer.Tags.length > 0
+            ? offer.Tags.map(({ Name }) => <Chip key={Name} style={{margin: '0 2px'}}>{Name}</Chip>)
+            : <i>Untagged</i>
+          }
         </div>
         <div style={{display: 'flex', alignItems: 'center'}}>
           <TimerIcon style={{marginRight: '0.5rem'}} /> {new Date(offer.ValidityPeriod).toString()}
--- components/Request.js
+++ components/Request.js
@@ -4,6 +4,7 @@ import AccountIcon from 'material-ui/svg-icons/action/account-circle'
 import LocationIcon from 'material-ui/svg-icons/communication/location-on'
 import TimerIcon from 'material-ui/svg-icons/image/timer'
 import LabelIcon from 'material-ui/svg-icons/action/label-outline'
+import Chip from 'material-ui/Chip';
 
 import { RequestPropType } from '../schemas/RequestSchema'
 import { toString } from "../helpers/Location"
@@ -26,7 +27,11 @@ export default class Request extends Component {
           <LocationIcon style={{marginRight: '0.5rem'}} /> {toString(request.Location)}
         </div>
         <div style={{display: 'flex', alignItems: 'center', marginBottom: '0.5rem'}}>
-          <LabelIcon style={{marginRight: '0.5rem'}} /> <i>Untagged</i>
+          <LabelIcon style={{marginRight: '0.5rem'}} />
+          {request.Tags && request.Tags.length > 0
+            ? request.Tags.map(({ Name }) => <Chip key={Name} style={{margin: '0 2px'}}>{Name}</Chip>)
+            : <i>Untagged</i>
+          }
         </div>
         <div style={{display: 'flex', alignItems: 'center'}}>
           <TimerIcon style={{marginRight: '0.5rem'}} /> {new Date(request.ValidityPeriod).toString()}
