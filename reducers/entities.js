87c37be8a53c3550beae8ef1a42c850467fc05a6
neopostmodern <clemens@neopostmodern.com>
2016-07-14 12:59:21 +0200

    Re-write entities reducer to be memory invariant when no relevant data  for entity type arrives

--- reducers/entities.js
+++ reducers/entities.js
@@ -1,25 +1,42 @@
-import merge from 'lodash/merge'
+import { combineReducers } from 'redux'
+
 import { REFRESH_LOGIN_FAILURE, LOGOUT_SUCCESS, LOGOUT_AFTER_TIMEOUT } from '../actions/login'
 
-// Updates an entity cache in response to any action with response.entities.
-export default function entities(state = { users: {}, offers: {}, requests: {}, matchings: {}, regions: {}, notifications: {}, tags: {} }, action) {
-  if (action.type == LOGOUT_SUCCESS || action.type == LOGOUT_AFTER_TIMEOUT || action.type == REFRESH_LOGIN_FAILURE) {
-    return entities(undefined, {});
-  }
-  if (action.response && action.response.entities) {
+const genericEntityReducer = function (entityName) {
+  return function (state = {}, action) {
+    if (action.type == LOGOUT_SUCCESS || action.type == LOGOUT_AFTER_TIMEOUT || action.type == REFRESH_LOGIN_FAILURE) {
+      return genericEntityReducer(entityName)(undefined, {});
+    }
+  
+    if (!action.response || !action.response.entities) {
+      return state;
+    }
+    
+    let entities = action.response.entities[entityName];
+    
+    if (!entities) {
+      return state;
+    }
+    
     let clonedState = Object.assign({}, state);
-    for (let entityType in action.response.entities) {
-      if (action.response.entities.hasOwnProperty(entityType)) {
-        let entityGroup = action.response.entities[entityType];
-        for (let entityId in entityGroup) {
-          if (entityGroup.hasOwnProperty(entityId)) {
-            clonedState[entityType][entityId] = entityGroup[entityId];
-          }
-        }
+    
+    for (let entityId in entities) {
+      if (entities.hasOwnProperty(entityId)) {
+        clonedState[entityId] = entities[entityId];
       }
     }
+      
     return clonedState;
   }
-  
-  return state;
-}
+};
+
+// Updates an entity cache in response to any action with response.entities.
+export default combineReducers({
+    users: genericEntityReducer('users'),
+    offers: genericEntityReducer('offers'),
+    requests: genericEntityReducer('requests'),
+    matchings: genericEntityReducer('matchings'),
+    regions: genericEntityReducer('regions'),
+    notifications: genericEntityReducer('notifications'),
+    tags: genericEntityReducer('tags')
+});
